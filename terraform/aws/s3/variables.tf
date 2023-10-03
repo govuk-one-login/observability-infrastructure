cead4176621dfbdd523fe8ed9cc7c0e4b423f049
melvabout <andrew.melvin@digital.cabinet-office.gov.uk>
2023-10-03 11:20:31 +0100

    Added validation for tag keys

--- terraform/aws/s3/variables.tf
+++ terraform/aws/s3/variables.tf
@@ -27,6 +27,26 @@ variable "bucket_name" {
 variable "tags" {
   description = "Tags to apply to created resources."
   type        = map(string)
+
+  validation {
+    condition     = contains(keys(var.tags), "Product")
+    error_message = "Tags must contain the Key Product."
+  }
+
+  validation {
+    condition     = contains(keys(var.tags), "System")
+    error_message = "Tags must contain the Key System."
+  }
+
+  validation {
+    condition     = contains(keys(var.tags), "Environment")
+    error_message = "Tags must contain the Key Environment."
+  }
+
+  validation {
+    condition     = contains(keys(var.tags), "Owner")
+    error_message = "Tags must contain the Key Owner."
+  }
 }
 
 variable "enable_versioning" {
