3c5846510d420ceea07137a9716d820d96d48854
melvabout <andrew.melvin@digital.cabinet-office.gov.uk>
2023-10-19 09:29:39 +0100

    Dynatrace provider is currently not needed

--- terraform/dynatrace/provider.tf
+++
@@ -1,8 +0,0 @@
-  terraform {
-      required_providers {
-          dynatrace = {
-              version = "1.44.0" # make sure I am up to date
-              source = "dynatrace-oss/dynatrace"
-          }
-      }
-  } 
