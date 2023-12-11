f9364e0515d354f55ad8f51473f6c252ea4dd72e
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2023-12-11 12:40:48 +0000

    Cleanup unused IaC

--- terraform/modules/secrets/main.tf
+++
@@ -1,9 +0,0 @@
-module "kms" {
-  source = "../../aws/kms"
-
-  aws_region                     = var.aws_region
-  kms_key_alias                  = local.name_string
-  kms_read_iam_policy_name       = local.kms_read_iam_policy_name
-  kms_read_write_iam_policy_name = local.kms_read_write_iam_policy_name
-  tags                           = local.tags
-}
--- terraform/site.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = ""
-    key            = "terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "terraform-state-lock-table"
-  }
-}
