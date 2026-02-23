e79664335abdfd11a7bff22f32171752f3ae5a4d
Jan Thomas Moldung <jan-thomas.moldung@digital.cabinet-office.gov.uk>
2026-02-23 14:28:57 +0000

    (feat)PSREGOV-3416 Use standarlized Checkov / pre-commit tests
    jira: [PSREGOV-3416]

--- infrastructure/gitsync-core/init/deploy.sh
+++ infrastructure/gitsync-core/init/deploy.sh
@@ -1,2 +1,16 @@
+#!/usr/bin/env bash
+
+TEMPLATE=./empty.yamlx
+
+if [[ ! -r ${TEMPLATE} ]] ; then
+  echo "ERROR: Can't see required template file '${TEMPLATE}' in current directory (${PWD})"
+  script_dir="$( realpath ${0%/*} )"
+  relative_script_dir="${script_dir#${PWD}}" # Remove the CWD part
+  # echo ${relative_script_dir#/}
+  # echo ".${relative_script_dir}"
+  echo "Maybe you need to 'cd .${relative_script_dir}'"
+  exit 1
+fi
+
 aws cloudformation deploy --stack-name aws-metric-streams-client --region eu-west-2 --template-file ./empty.yaml
 echo "STATUS: Stack deploy complete."
