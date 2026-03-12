204215ed5b6f3092d81a7fe6c5c92a2c16c13b8e
Rory Sedgwick <rory.sedgwick@digital.cabinet-office.gov.uk>
2026-03-12 15:03:33 +0000

    PSREGOV-3781: remove obsolete test tag

--- infrastructure/gitsync-core/codepipeline/deploy.sh
+++ infrastructure/gitsync-core/codepipeline/deploy.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
-ENVIRONMENT=${1}
-# WORKING_DIR="infrastructure/gitsync-core/step-1/template.yaml,infrastructure/gitsync-core/step-2/notification-rules.yaml,infrastructure/gitsync-core/step-3/slack-integration.yaml"
-WORKING_DIR="infrastructure/gitsync-core/$ENVIRONMENT"  # this is incorrect but what is currently set in existing stacks
+ENVIRONMENT=${1:-development}
+CONNECTIONNAME=${2:-GDS-GitHub-Connection}
 
-# dev connection ARN, needs to be paramaterised
-CODECONNECTION_ARN="arn:aws:codestar-connections:eu-west-2:975050370687:connection/2a0bb694-3010-421b-a31c-45d9b9fc179e"
+echo "INFO: collecting CodeConnection ARN"
+CONNECTIONARN=$(
+  aws codestar-connections list-connections \
+    --query "Connections[?ConnectionName=='${CONNECTIONNAME}'].ConnectionArn" \
+    --output text \
+    --region eu-west-2)
+
+echo "INFO: Using the CodeConnection: (${CONNECTIONARN})"
+
+# WORKING_DIR="infrastructure/gitsync-core/step-0/template.yaml, infrastructure/gitsync-core/step-1/template.yaml, infrastructure/gitsync-core/step-2/notification-rules.yaml, infrastructure/gitsync-core/step-3/slack-integration.yaml"
+WORKING_DIR="infrastructure/gitsync-core/*/*.yaml"
 
 aws cloudformation deploy \
     --region eu-west-2 \
@@ -16,4 +24,4 @@ aws cloudformation deploy \
     --parameter-overrides \
         CTEnvironment="${ENVIRONMENT}" \
         WorkingDir="${WORKING_DIR}" \
-        ConnectionArn="${CODECONNECTION_ARN}"
+        ConnectionArn="${CONNECTIONARN}"
